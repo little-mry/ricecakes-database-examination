30b1f519c7be54f74db4c9c30c15d2c77738d976
Lilla Mry <maria.kjellholm@gmail.com>
2025-05-21 10:54:30 +0200

    fixat stavfel i joi-scheman

--- src/validation/userValidation.js
+++ src/validation/userValidation.js
@@ -3,20 +3,20 @@ import joi, { any } from "joi";
 export const createUserSchema = joi.object({
   firstname: joi.string().min(1).max(20).required().messages({
     "string.base": "Firstname måste vara en sträng",
-    "string-min": "Firstname måste bestå av minst {#limit} tecken",
-    "string-max": "Firstname får bestå av max {#limit} tecken",
+    "string.min": "Firstname måste bestå av minst {#limit} tecken",
+    "string.max": "Firstname får bestå av max {#limit} tecken",
     "any.required": "Firstname saknas",
   }),
   lastname: joi.string().min(1).max(50).required().messages({
     "string.base": "Lastname måste vara en sträng",
-    "string-min": "Lastname måste bestå av minst {#limit} tecken",
-    "string-max": "Lastname får bestå av max {#limit} tecken",
+    "string.min": "Lastname måste bestå av minst {#limit} tecken",
+    "string.max": "Lastname får bestå av max {#limit} tecken",
     "any.required": "Lastname saknas",
   }),
   username: joi.string().min(3).max(30).required().messages({
     "string.base": "Username måste vara en sträng",
-    "string-min": "Username måste bestå av minst {#limit} tecken",
-    "string-max": "Username får bestå av max {#limit} tecken",
+    "string.min": "Username måste bestå av minst {#limit} tecken",
+    "string.max": "Username får bestå av max {#limit} tecken",
     "any.required": "Username saknas",
   }),
   email: joi.string().email().required().messages({
@@ -34,8 +34,8 @@ export const createUserSchema = joi.object({
 export const loginUserSchema = joi.object({
   username: joi.string().min(3).max(30).required().messages({
     "string.base": "Username måste vara en sträng",
-    "string-min": "Username måste bestå av minst {#limit} tecken",
-    "string-max": "Username får bestå av max {#limit} tecken",
+    "string.min": "Username måste bestå av minst {#limit} tecken",
+    "string.max": "Username får bestå av max {#limit} tecken",
     "any.required": "Username saknas",
   }),
   password: joi.string().min(6).max(50).required().messages({
